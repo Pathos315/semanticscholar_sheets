b523eb2fa244e942796bf5dc03252053810d4679
Pathos315 <john.fallot@gmail.com>
2022-07-23 11:47:21 -0400

    improved type hinting

--- semanticscholargooglesheets.js
+++ semanticscholargooglesheets.js
@@ -7,16 +7,17 @@ function getResponse(url) {
 /**
  * Return the keywords of a paper using from the Dimensions.ai  based on a query.
  *
- * @param {input}  input  The query string sent to the Dimensions.ai API url.
- * @returns {doi} The keywords, as a list.
+ * @param {string}  input The query string sent to the Dimensions.ai API url.
+ * @param {string}  output The field to be selected from the data
+ * @returns {string}  result  The keywords, as an array formatted as a string.
  * @customfunction
  */
 function GETDIMENSIONS(input = 'prosocial', output = "mesh_terms") {
-    var dataset_url = `https://app.dimensions.ai/discover/publication/results.json?search_mode=content&search_text=${input}&search_type=kws&search_field=doi`;
+    const dataset_url = `https://app.dimensions.ai/discover/publication/results.json?search_mode=content&search_text=${input}&search_type=kws&search_field=doi`;
     var data = getResponse(dataset_url);
     for (const [key, value] of Object.entries(data.docs[0])) {
         if (key === output) {
-            var result = `${value}`;
+            var result = value;
             Logger.log(result);
             return result
         };
@@ -26,15 +27,15 @@ function GETDIMENSIONS(input = 'prosocial', output = "mesh_terms") {
 /**
  * Return the authors of a paper, as a string, from the Semantic Scholar API based on a query.
  *
- * @param {input}  input  The query string sent to the Semantic Scholar API url.
- * @returns {doi}  doi  The paper's authors, as a string.
+ * @param {string}  input  The query string sent to the Semantic Scholar API url. Defaults to 'prosocial'.
+ * @returns {string}  The paper's authors, as a string.
  * @customfunction
  */
 function GETPAPERAUTHORS(input = 'prosocial') {
     /* Gets the API from the provided URL
     then gets the response */
     ;
-    var api_url = `https://api.semanticscholar.org/graph/v1/paper/search?query=${input}&limit=1&fields=authors`;
+    const api_url = `https://api.semanticscholar.org/graph/v1/paper/search?query=${input}&limit=1&fields=authors`;
     var data = getResponse(api_url);
     var authors = data.data[0].authors;
     var output = ""
@@ -51,12 +52,12 @@ function GETPAPERAUTHORS(input = 'prosocial') {
  * Return the digital object identifier (DOI) value of a paper from the Semantic Scholar API based on a query.
  * Initial use shows current time.
  *
- * @param {"input"}  input  The query string sent to the Semantic Scholar API url.
+ * @param {string} input The query string sent to the Semantic Scholar API url.
  * @returns {doi} The Digital Object Identifier (DOI) value of the requested paper.
  * @customfunction
  */
 function GET_DIGITALOBJECTIDENTIFIER(input) {
-    var api_url = `https://api.semanticscholar.org/graph/v1/paper/${input}?fields=externalIds`;
+    const api_url = `https://api.semanticscholar.org/graph/v1/paper/${input}?fields=externalIds`;
     var data = getResponse(api_url);
     for (const [key, value] of Object.entries(data.externalIds)) {
         if (key === 'DOI') {
@@ -71,14 +72,13 @@ function GET_DIGITALOBJECTIDENTIFIER(input) {
  * Return a given data field from the Semantic Scholar API based on its Semantic Scholar ID or 'sha'.
  * Initial use shows current time.
  *
- * @param {"649def34f8be52c8b66281af98ae884c09aef38b"}  sha  Semantic Scholar ID string to query the Semantic Scholar API.
- * @param {"title"} field The Object key to be queried, it must be one of the fields listed in the Semantic Scholar API documentation.
+ * @param {string}  sha  Semantic Scholar ID string to query the Semantic Scholar API.
+ * @param {string} field The Object key to be queried, it must be one of the fields listed in the Semantic Scholar API documentation.
  * @returns {output} The values that were requested based on the provided key.
  * @customfunction
  */
 function GETDETAILSFROMSHA(sha = '649def34f8be52c8b66281af98ae884c09aef38b', field = 'title') {
-
-    var api_url = `https://api.semanticscholar.org/graph/v1/paper/${sha}?fields=${field}`;
+    const api_url = `https://api.semanticscholar.org/graph/v1/paper/${sha}?fields=${field}`;
     var data = getResponse(api_url);
     try {
         var output = data.data[0][`${field}`];
@@ -93,19 +93,17 @@ function GETDETAILSFROMSHA(sha = '649def34f8be52c8b66281af98ae884c09aef38b', fie
  * Return a given data field from the Semantic Scholar API based on a query, such as keywords
  * Initial use shows current time.
  *
- * @param {"covid+vaccination"}  input  The query string sent to the Semantic Scholar API url.
- * @param {"title"} field The Object key to be queried, it must be one of the fields listed in the Semantic Scholar API documentation.
- * @returns {output} The values that were requested based on the provided key.
+ * @param {string}  input  The query string sent to the Semantic Scholar API url. Example: "covid+vaccination"
+ * @param {string} field The Object key to be queried as a string, it must be one of the fields listed in the Semantic Scholar API documentation.
+ * @returns {Promise<T>} output The values that were requested based on the provided key.
  * @customfunction
  */
 
 async function GETDETAILSFROMQUERY(input = 'prosocial', field = 'title') {
-    var api_url = `https://api.semanticscholar.org/graph/v1/paper/search?query=${input}&limit=1&fields=${field}`;
-    var response = UrlFetchApp.fetch(api_url);
-    var json = response.getContentText();
-    var data = JSON.parse(json);
+    const api_url = `https://api.semanticscholar.org/graph/v1/paper/search?query=${input}&limit=1&fields=${field}`;
+    var data = getResponse(api_url);
     try {
-        var output = data.data[0][`${field}`];
+        var output = data.data[0][field];
     } catch (e) {
         var output = `No data found for field. Reason: ${e}.`;
     };
